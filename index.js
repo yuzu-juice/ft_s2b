03a8b1fea6d2764c85389ad0e5dd600c330d3390
yuzu-juice <kitazato48@gmail.com>
2024-05-24 03:02:09 +0900

    update

--- index.js
+++ index.js
@@ -8,18 +8,3 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
-
-// ヘッダとフッタを動的に読み込む関数
-function loadComponent(url, elementId) {
-    fetch(url)
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById(elementId).innerHTML = data;
-        });
-}
-
-// ページが読み込まれた後にヘッダとフッタを読み込む
-document.addEventListener("DOMContentLoaded", function() {
-    loadComponent("header.html", "header-placeholder");
-    loadComponent("footer.html", "footer-placeholder");
-});
